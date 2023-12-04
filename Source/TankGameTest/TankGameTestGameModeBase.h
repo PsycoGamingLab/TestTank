e23c276f24fdf9379f403abd269fda88d037fe62
PsycoGamingLab <psycogaminglab@gmail.com>
2023-12-04 16:53:00 +0100

    Added project

---
+++ Source/TankGameTest/TankGameTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TankGameTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAMETEST_API ATankGameTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
