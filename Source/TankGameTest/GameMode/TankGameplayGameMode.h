67624c5c40b90d197b87c948070783fbd328d9a5
PsycoGamingLab <psycogaminglab@gmail.com>
2023-12-04 17:15:26 +0100

    added c++ classes

---
+++ Source/TankGameTest/GameMode/TankGameplayGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "TankGameplayGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAMETEST_API ATankGameplayGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/TankGameTest/GameState/TankGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "TankGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAMETEST_API ATankGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
